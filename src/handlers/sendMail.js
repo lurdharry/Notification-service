c6d02014833f329afc7aba89f631ec54a25e8ca4
lurdharry <azeezhammed57@gmail.com>
2022-03-22 01:29:10 +0100

    chore: test send email

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,10 +1,31 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "us-west-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
-  // {
-  //   statusCode: 200,
-  //   body: JSON.stringify({ message: "Hello from https://codingly.io" }),
-  // };
+  const params = {
+    Source: "azeezhammed57@gmail.com",
+    Destination: {
+      ToAddresses: ["azeezhammed57@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello",
+        },
+      },
+      Subject: {
+        Data: "Test Mail",
+      },
+    },
+  };
+  try {
+    const result = ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const handler = sendMail;
